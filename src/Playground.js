5de7ff69550af170fdac4fd72e99652059a31fca
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-09-12 19:45:46 -0500

    Work in progress WindowSize.js

--- src/Playground.js
+++ src/Playground.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import randomColor from 'randomcolor';
 
 export default function Playground() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(30)
   
+  const inputRef = useRef();
+
   const [color, setColor] = useState(null);
   
   useEffect(() => {
     setColor(randomColor());
+    inputRef.current.focus();
   }, [count]); //<- this parameter makes the conditional of which variable needs to change on the render so to make the useEffect based on that
 
   return (
@@ -21,6 +24,12 @@ export default function Playground() {
         setCount(currentCount => currentCount + 1)}>
         +
       </button>  
+      <hr />
+      <input 
+        ref={inputRef}
+        type="range" 
+        onChange={e => setCount(e.target.value)} 
+        value={count} />
     </div>
   )
 }
--- src/components/Paint.js
+++ src/components/Paint.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Name from './Name'
 import Canvas from './Canvas'
 import ColorPicker from './ColorPicker'
+import WindowSize from './WindowSize'
 import randomColor from 'randomcolor'
 
 export default function Paint() {
-  const [colors, setColors] = useState([]);
-  const [activeColor, setActiveColor] = useState(null);
-
+  const [colors, setColors] = useState([])
+  const [activeColor, setActiveColor] = useState(null)
   const getColors = () => {
     const baseColor = randomColor().slice(1);
     fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
@@ -17,12 +17,14 @@ export default function Paint() {
       setActiveColor(res.colors[0].hex.value)
     })
   }
-
-  useEffect(getColors, []);
+  useEffect(getColors, [])
+  
+  const headerRef = useRef({ offsetHeight: 0 })
+  
   return (
     <div className="app">
-      <header style={{ borderTop: `10px solid ${activeColor}` }}>
-        <div className="app">
+      <header ref={headerRef} style={{ borderTop: `10px solid ${activeColor}` }}>
+        <div>
           <Name />
         </div>
         <div style={{ marginTop: 10 }}>
@@ -36,9 +38,10 @@ export default function Paint() {
       {activeColor && (
         <Canvas
           color={activeColor}
-          height={window.innerHeight}
+          height={window.innerHeight - headerRef.current.offsetHeight}
         />
       )}
+      <WindowSize />
     </div>
   )
 }
